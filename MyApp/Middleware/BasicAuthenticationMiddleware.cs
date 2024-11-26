9e0dffe828831055d6034e61ed974e1f28336483
Sujeeth KK <101649038+sujeethskk110@users.noreply.github.com>
2024-11-26 15:49:03 +0530

    Add project files.

---
+++ MyApp/Middleware/BasicAuthenticationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using MyApp.MiddlewareInterface;
+
+namespace MyApp.Middleware
+{
+    public class BasicAuthenticationMiddleware : IAuthenticationMiddeleware
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly RequestDelegate _next;
+        private readonly ILogger<BasicAuthenticationMiddleware> _logger;
+        private readonly IConfiguration _configuration;
+
+
+        public BasicAuthenticationMiddleware(RequestDelegate next, IHttpContextAccessor contextAccessor, ILogger<BasicAuthenticationMiddleware> logger, IConfiguration configuration )
+        {
+            _contextAccessor = contextAccessor;
+            _next = next;
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string authHeader = context.Request.Headers["Authorization"];
+            if (authHeader != null && authHeader.StartsWith("Basic"))
+            {
+                string encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.
+    }
+}
